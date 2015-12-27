abb0e4220d24ec2cf5c1d87ae695a9c159e2d130
ik9999 <mailm743@yahoo.com>
2015-12-27 23:19:26 +0300

    release task

--- tasks/release.js
+++ tasks/release.js
@@ -5,24 +5,23 @@ let packageJson = require('../package.json');
 
 let buildDir = path.join(__dirname, '..', 'build');
 let resDir = path.join(__dirname, '..', 'release_res');
-let nodeModulesDir = path.join(__dirname, '..', 'node_modules');
 let releaseDir = path.join(__dirname, '..', 'release');
 let releaseVersionDir = path.join(releaseDir, packageJson.version);
 let releaseBuildDir = path.join(releaseVersionDir, 'dist');
-let releaseNodeModulesDir = path.join(releaseVersionDir, 'node_modules');
+
+let packageJsonPath = path.join(__dirname, '..', 'package.json');
+let packageJsonReleasePath = path.join(releaseVersionDir, 'package.json');
 
 gulp.task('createDirs', () => {
   fsJ.dir(releaseDir, {empty: true});
   fsJ.dir(releaseVersionDir, {empty: true});
   fsJ.dir(releaseBuildDir, {empty: true});
-  fsJ.dir(releaseNodeModulesDir, {empty: true});
 });
 
 gulp.task('release', ['createDirs'], () => {
   fsJ.file(path.join(releaseVersionDir, 'run.sh'), {mode: '775'});
+  fsJ.file(path.join(releaseVersionDir, 'setup.sh'), {mode: '775'});
   fsJ.copy(resDir, releaseVersionDir, {overwrite: true});
   fsJ.copy(buildDir, releaseBuildDir, {overwrite: true});
-  Object.keys(packageJson.dependencies).forEach(moduleName => {
-    fsJ.copy(path.join(nodeModulesDir, moduleName), path.join(releaseNodeModulesDir, moduleName), {overwrite: true});
-  });
+  fsJ.copy(packageJsonPath, packageJsonReleasePath, {overwrite: true});
 });
